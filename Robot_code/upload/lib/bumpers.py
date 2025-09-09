7d0b9eed91666aa5dbfb4366ff2d751b0e6c4fce
Dieter <dieter.vanderelst@fastmail.net>
2025-09-09 11:30:26 -0400

    updated bumpers so I can set threshold

--- Robot_code/upload/lib/bumpers.py
+++ Robot_code/upload/lib/bumpers.py
@@ -5,15 +5,17 @@ import time
 class Bumpers:
     def __init__(self, leds=None):
         self._bump = ir_sensors.BumpSensors()
-        self._bump.reset_calibration()
-        self._bump.calibrate()
         self.leds = leds
+        self.left_threshold = 250
+        self.right_threshold = 250
 
     def read(self,update_leds=True):
         """Read the bump sensors and return a (left, right) tuple of booleans."""
-        self._bump.read()
-        bump_left = self.left()
-        bump_right = self.right()
+        values = self._bump.read()
+        left_value = values[0]
+        right_value = values[1]
+        bump_left = left_value > self.left_threshold
+        bump_right = right_value > self.right_threshold
         if update_leds and self.leds is not None:
             self.leds.set(5, 'red') if bump_left else self.leds.set(5, 'black')
             self.leds.set(3, 'red') if bump_right else self.leds.set(3, 'black')
